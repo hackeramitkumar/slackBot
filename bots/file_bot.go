a174c4d27a495e26f1bb8b94129281cea590dcf7
hackeramitkumar <amit9116260192@gmail.com>
2023-10-04 17:20:15 +0530

    added file bot
    Signed-off-by: hackeramitkumar <amit9116260192@gmail.com>

---
+++ bots/file_bot.go
@@ -0,0 +1,31 @@
+package bot
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+const (
+	amit = "hello"
+)
+
+func upload_file() {
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"DS.pdf", "CC57.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+}
---
+++ main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	bot.upload_file()
+}
